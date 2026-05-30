5a8134de0bc454153eade0dd0252b92a8c1d9561
mboji <mbojic12@gmail.com>
2026-05-30 12:49:46 +0200

    stuff

--- main.py
+++ main.py
@@ -79,9 +79,9 @@ Repeat this block for each step in sequence.
 """.strip()
 
 url = "https://api.deepseek.com"
-modelName = "deepseek-v4-flash"
-# modelName = "deepseek-v4-pro"
-api_key = "sk-d6dbea0408e9471da7a71af4496634b4"
+# modelName = "deepseek-v4-flash"
+modelName = "deepseek-v4-pro"
+api_key = "sk-"
 
 client = OpenAI(
     api_key=api_key,
